48b3594f299e4df02537baa2a9f26161970f730d
Baris Soner Usakli <barisusakli@gmail.com>
2014-01-24 23:49:00 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# NodeBB Link Checker
+
+Checks if links in posts are still alive. It puts a broken chain icon next to the link if it is broken.
+
+## Installation
+
+    npm install nodebb-plugin-linkcheck
+
+![](http://i.imgur.com/Nt9N1Zo.png)
+
+
---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+(function(module) {
+	"use strict";
+
+
+	module.parse = function(postContent, callback) {
+		var regex = /\S*#(?:\[[^\]]+\]|\S+[^<\/p> ])/g;
+
+		var matches = postContent.match(regex);
+		if(!matches || !matches.length) {
+			return callback(null, postContent);
+		}
+
+		for(var i=0; i<matches.length; ++i) {
+
+			var match = matches[i].replace('</p>', '').trim();
+			console.log('"' + match + '"');
+			postContent = postContent.replace(match, '<a href="https://twitter.com/search?q=%40'+ match.slice(1) +'">' +
+				match + '</a>');
+
+		}
+
+		return callback(null, postContent);
+	}
+
+
+}(module.exports));
+
