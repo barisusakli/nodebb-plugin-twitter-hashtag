e176bc6cc027dde4ebd124a36addb86ed0a92205
Baris Soner Usakli <barisusakli@gmail.com>
2014-01-24 23:56:34 -0500

    removed console.log

--- index.js
+++ index.js
@@ -13,12 +13,9 @@
 		}
 
 		for(var i=0; i<matches.length; ++i) {
-
 			var match = matches[i].replace('</p>', '').trim();
-			console.log('"' + match + '"');
 			postContent = postContent.replace(match, '<a href="https://twitter.com/search?q=%40'+ match.slice(1) +'">' +
 				match + '</a>');
-
 		}
 
 		return callback(null, postContent);
