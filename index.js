6bb582d02c4c05bdedc1a0f9d7e6ba61cda4d0c4
barisusakli <barisusakli@gmail.com>
2014-11-05 18:00:11 -0500

    update for new hooks in 0.6.0

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 'use strict';
 
 (function(module) {
-	module.parse = function(postContent, callback) {
-		var regex = /(?:\s|^)(?:#(?!\d+(?:\s|$)))(\w+)(?=\s|$)/gi;
+	var regex = /(?:\s|^)(?:#(?!\d+(?:\s|$)))(\w+)(?=\s|$)/gi;
 
-		var matches = postContent.match(regex);
+	module.parse = function(data, callback) {
+		if (!data || !data.postData || !data.postData.content) {
+			return callback(null, data);
+		}
+
+		var matches = data.postData.content.match(regex);
 		if(!matches || !matches.length) {
-			return callback(null, postContent);
+			return callback(null, data);
 		}
 
 		for(var i=0; i<matches.length; ++i) {
 			var match = matches[i].replace('</p>', '').trim();
-			postContent = postContent.replace(match, '<a href="https://twitter.com/search?q=%40'+ match.slice(1) +'">' +
+			data.postData.content = data.postData.content.replace(match, '<a href="https://twitter.com/search?q=%40'+ match.slice(1) +'">' +
 				match + '</a>');
 		}
 
-		callback(null, postContent);
+		callback(null, data);
 	};
 }(module.exports));
 
